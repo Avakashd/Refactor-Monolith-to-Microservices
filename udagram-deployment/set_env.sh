03764e632dad220edf0e3222b3262b707145726f
Avakash <63589180+Avakashd@users.noreply.github.com>
2021-06-04 18:54:49 +0530

    Add files via upload

--- udagram-deployment/set_env.sh
+++ udagram-deployment/set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=avakashd
 export POSTGRES_PASSWORD=avakashd
-export POSTGRES_HOST=avakashd.c8eqyu2gosg1.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=avakashd-rds.cfywob1wrqgf.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=avakashd-dev
-export AWS_REGION=us-east-1
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=hello
 export URL=http://localhost:8100
